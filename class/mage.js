bb80c1d6bec1d1113654aa3f6cb6772867053a7e
TimotheGury <gury.timothe@gmail.com>
2025-03-07 10:01:35 +0100

    mage

---
+++ class/mage.js
@@ -0,0 +1,25 @@
+import Character from "./character";
+class Mage extends Character {
+  constructor(power, name, pv, mana) {
+    this.mana = mana;
+  }
+
+  attack(target) {
+    if (this.mana > 0) {
+      super.attack(target);
+      this.mana--;
+      console.log(
+        `${this.name} attaque ${target.name} et lui inflige ${this.power} dégâts ! Mana restant : ${this.mana}`
+      );
+    } else {
+      console.log(`${this.name} n'as plus de mana pour attaquer !`);
+    }
+  }
+
+  recoverEndurance(amount) {
+    this.mana += amount;
+    console.log(`${this.name} récupère ${amount} points de mana.`);
+  }
+}
+
+module.exports = Character;
