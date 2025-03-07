9bc81f880b20518fd7b968e32c06fcd179f1f1c2
Anissa <anissa.elbouazzaoui@gmail.com>
2025-03-07 10:48:55 +0100

    Ajout personnage

--- class/character.js
+++ class/character.js
@@ -1,14 +1,25 @@
 class Character {
-    constructor(power, name, pv) {
+    constructor(name, power,  pv, endurance, mana) {
         this.power = power;
         this.name = name;
         this.pv = pv;
+        this.mana = mana;
+        this.endurance = endurance;
+        this.attaques = [];
         
     }
 
-    attack(target) {
-        target.pv -= this.power;
-    }
+    ajouterAttaque(attaque) {
+        this.attaques.push(attaque);
+      }
+    
+      attaquer(index) {
+        if (this.attaques[index]) {
+          this.attaques[index].utiliser(this);
+        } else {
+          console.log("Cette attaque n'existe pas !");
+        }
+      }
 
     takeDamage(damage) {
         this.pv -= damage
@@ -18,5 +29,14 @@ class Character {
         return this.pv > 0;
     }
 }
+const mage = new Personnage("Gandalf", 100, 205, 0, 100);
+const guerrier = new Personnage("Conan", 220, 100, 100, 0);
+const paladin = new Personnage("Frost", 220, 100, 50, 50);
+
+// Attribution des attaques
+mage.ajouterAttaque(eclairMagique);
+guerrier.ajouterAttaque(coupDepee);
+paladin.ajouterAttaque(coupDepee, eclairMagique);
+
 
 module.exports = Character
