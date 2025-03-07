cf03058c62a9836ccb7e1067ee28a07f7eb80ecd
Yann <yann.sady.pro@gmail.com>
2025-03-07 11:45:53 +0100

    add regina

---
+++ tp-polymorphisme/pizzeria/class/regina.js
@@ -0,0 +1,15 @@
+const Pizza = require('./pizza');
+const Ingredient = require('./ingredient');
+
+class Regina extends Pizza {
+    constructor() {
+        super('Regina', 'pâte fine', 'tomate');
+        this.tag = 'en attente';
+        this.ajouterIngredient(new Ingredient('jambon', 2.5));
+        this.ajouterIngredient(new Ingredient('champignons', 1.8));
+        this.ajouterIngredient(new Ingredient('mozzarella', 2));
+        this.ajouterIngredient(new Ingredient('olives', 0.5));
+    }
+}
+
+module.exports = Reine;
